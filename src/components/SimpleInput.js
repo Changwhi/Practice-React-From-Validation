97e1d548280e9a8ab5a8ff85df8d4eeaabf117da
Changwhi <whinada@gmail.com>
2023-07-21 12:24:00 -0700

    refactor: add enteredNameTouched useState to avoid displaying error message right after rendering

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -4,14 +4,16 @@ import { useRef, useState } from 'react';
 const SimpleInput = (props) => {
     const nameInputRef = useRef();
     const [enteredName, setEnteredName] = useState('');
-    const [enteredNameIsValid, setEnteredNameIsValid] = useState(true);
-
+    const [enteredNameIsValid, setEnteredNameIsValid] = useState(false);
+    const [entereNameTouched, setEnteredNameTouched] = useState(false);
     const nameInputChangeHandler = event => {
-       setEnteredName(event.target.value);
+        setEnteredName(event.target.value);
     };
 
     const formSubmissionHandler = event => {
         event.preventDefault();
+
+        setEnteredNameTouched(true);
 
         if (enteredName.trim() === '') {
             setEnteredNameIsValid(false);
@@ -27,19 +29,22 @@ const SimpleInput = (props) => {
         setEnteredName("");
     };
 
-  const nameInputClasses = enteredNameIsValid ? 'form-control' : 'form-control invalid'
-  return (
-    <form onSubmit={formSubmissionHandler}>
-      <div className={nameInputClasses}>
-        <label htmlFor='name'>Your Name</label>
-        <input ref={nameInputRef} type='text' id='name' value={enteredName} onChange={nameInputChangeHandler}/>
-      {!enteredNameIsValid && <p className='error-text'> Name must not be empty. </p>}
-      </div>
-      <div className="form-actions">
-        <button>Submit</button>
-      </div>
-    </form>
-  );
+    const nameInputIsInvalid = !enteredNameIsValid && enteredNameIsValid;
+
+    const nameInputClasses = nameInputIsInvalid ? 'form-control invalid' : 'form-contro'
+
+    return (
+        <form onSubmit={formSubmissionHandler}>
+            <div className={nameInputClasses}>
+                <label htmlFor='name'>Your Name</label>
+                <input ref={nameInputRef} type='text' id='name' value={enteredName} onChange={nameInputChangeHandler} />
+                {enteredNameIsValid && <p className='error-text'> Name must not be empty. </p>}
+            </div>
+            <div className="form-actions">
+                <button>Submit</button>
+            </div>
+        </form>
+    );
 };
 
 export default SimpleInput;
